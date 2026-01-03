82de758455fafc472520e8932b054827f9849729
RChaubey16 <ruturajchaubey16@gmail.com>
2026-01-03 21:09:40 +0530

    chore: add vercel-fe domain to cors config

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: "http://localhost:3001",
+    origin: ["https://knowbase-app.vercel.app", "http://localhost:3001"],
     credentials: true,
   });
 
